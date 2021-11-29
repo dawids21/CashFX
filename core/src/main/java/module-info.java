dd5529b2aa978798de2eadb5280bcaec308cd08d
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-11-29 14:41:32 +0100

    create module-info.java for core module

---
+++ core/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module xyz.stasiak.cashfxcore {
+    requires io.vavr;
+
+    exports xyz.stasiak.cashfx.account;
+    exports xyz.stasiak.cashfx.account.exceptions;
+    exports xyz.stasiak.cashfx.user;
+    exports xyz.stasiak.cashfx.user.exception;
+    exports xyz.stasiak.cashfx.context;
+}
