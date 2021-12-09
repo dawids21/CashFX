b4c9ca1bf7ae7ced2ae845cb778f99a6f73a842d
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-12-09 21:31:59 +0100

    return id when creating user

--- core/src/main/java/xyz/stasiak/cashfx/user/UserApplicationService.java
+++ core/src/main/java/xyz/stasiak/cashfx/user/UserApplicationService.java
@@ -10,13 +10,13 @@ public class UserApplicationService {
         this.repository = repository;
     }
 
-    User create(String name, String surname) {
+    public int create(String name, String surname) {
         var user = User.create(name, surname);
 
-        return repository.save(user);
+        return repository.save(user).toReadModel().id();
     }
 
-    UserReadModel getById(Integer id) {
+    public UserReadModel getById(Integer id) {
         return repository.getById(id)
                 .getOrElseThrow(() -> new UserNotFound(id));
     }
