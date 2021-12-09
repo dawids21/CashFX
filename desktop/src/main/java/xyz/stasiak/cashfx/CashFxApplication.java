3788892710a023e68742b42f6d531b55cafb9764
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-12-09 22:26:10 +0100

    create user list

--- desktop/src/main/java/xyz/stasiak/cashfx/CashFxApplication.java
+++ desktop/src/main/java/xyz/stasiak/cashfx/CashFxApplication.java
@@ -5,8 +5,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import xyz.stasiak.cashfx.account.AccountApplicationService;
 import xyz.stasiak.cashfx.account.AccountConfig;
 import xyz.stasiak.cashfx.context.ApplicationContext;
+import xyz.stasiak.cashfx.user.UserApplicationService;
 import xyz.stasiak.cashfx.user.UserConfig;
 
 import java.io.IOException;
@@ -21,7 +23,12 @@ public class CashFxApplication extends Application {
         ApplicationContext.init(List.of(
                 new AccountConfig(), new UserConfig()
         ));
-        FXMLLoader fxmlLoader = new FXMLLoader(CashFxApplication.class.getResource("hello-view.fxml"));
+        var userApplicationService = ApplicationContext.CONTEXT.getBean(UserApplicationService.class);
+        var accountApplicationService = ApplicationContext.CONTEXT.getBean(AccountApplicationService.class);
+        var userId = userApplicationService.create("Dawid", "Stasiak");
+        var account = accountApplicationService.openBasic(userId, "Basic");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(CashFxApplication.class.getResource("cash-fx-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("CashFX");
         stage.setScene(scene);
--- desktop/src/main/java/xyz/stasiak/cashfx/CashFxController.java
+++ desktop/src/main/java/xyz/stasiak/cashfx/CashFxController.java
@@ -1,14 +1,26 @@
 package xyz.stasiak.cashfx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import xyz.stasiak.cashfx.context.ApplicationContext;
+import xyz.stasiak.cashfx.user.UserApplicationService;
+import xyz.stasiak.cashfx.user.UserReadModel;
 
 public class CashFxController {
+
+    private final ObservableList<UserReadModel> observableList = FXCollections.observableArrayList();
     @FXML
-    private Label welcomeText;
+    private ListView<UserReadModel> userList;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void initialize() {
+        var service = ApplicationContext.CONTEXT.getBean(UserApplicationService.class);
+        observableList.addAll(service.getAllUsers().toJavaList());
+        userList.setItems(observableList);
+
+        userList.setCellFactory(userReadModelListView -> new UserListCell());
     }
+
 }
