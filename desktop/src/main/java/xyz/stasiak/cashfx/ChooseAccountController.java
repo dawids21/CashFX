17fc539c84203fb43359239d6e7e70012cda7d48
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-12-12 13:46:36 +0100

    handle login button

---
+++ desktop/src/main/java/xyz/stasiak/cashfx/ChooseAccountController.java
@@ -0,0 +1,11 @@
+package xyz.stasiak.cashfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import xyz.stasiak.cashfx.account.AccountReadModel;
+
+public class ChooseAccountController {
+
+    @FXML
+    private ListView<AccountReadModel> accountsList;
+}
---
+++ desktop/src/main/java/xyz/stasiak/cashfx/ChooseUserController.java
@@ -0,0 +1,48 @@
+package xyz.stasiak.cashfx;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+import xyz.stasiak.cashfx.context.ApplicationContext;
+import xyz.stasiak.cashfx.user.UserApplicationService;
+import xyz.stasiak.cashfx.user.UserReadModel;
+
+import java.io.IOException;
+
+public class ChooseUserController {
+
+    private final ObservableList<UserReadModel> observableList = FXCollections.observableArrayList();
+    private final UserApplicationService service;
+    private final ApplicationState applicationState;
+    @FXML
+    private ListView<UserReadModel> userList;
+
+    public ChooseUserController() {
+        service = ApplicationContext.CONTEXT.getBean(UserApplicationService.class);
+        applicationState = ApplicationContext.CONTEXT.getBean(ApplicationState.class);
+    }
+
+    @FXML
+    void initialize() {
+        observableList.addAll(service.getAllUsers().toJavaList());
+        userList.setItems(observableList);
+        userList.setCellFactory(userReadModelListView -> new UserListCell());
+    }
+
+    @FXML
+    void onLoginButtonAction(ActionEvent event) throws IOException {
+        var userId = userList.getSelectionModel().getSelectedItem().id();
+        applicationState.setUserId(userId);
+        var stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        var fxmlLoader = new FXMLLoader(getClass().getResource("choose-account-view.fxml"));
+        var scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setScene(scene);
+    }
+
+}
