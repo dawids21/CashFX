5fb4dcfdb7ce3456554a936094b6fac8c2bd62c0
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-12-13 17:58:47 +0100

    create account details view

---
+++ desktop/src/main/java/xyz/stasiak/cashfx/AccountDetailsController.java
@@ -0,0 +1,61 @@
+package xyz.stasiak.cashfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AccountDetailsController {
+
+    @FXML
+    private Label nameLabel;
+    @FXML
+    private Label moneyLabel;
+    @FXML
+    private Label chargeLabel;
+    @FXML
+    private Label typeLabel;
+
+    @FXML
+    void initialize() {
+
+    }
+
+    @FXML
+    void onLogoutButtonAction() {
+
+    }
+
+    @FXML
+    void onChangeAccountButtonAction() {
+
+    }
+
+    @FXML
+    void onTransferButtonAction() {
+
+    }
+
+    @FXML
+    void onPayChargeButtonAction() {
+
+    }
+
+    @FXML
+    void onTakeLoanButtonAction() {
+
+    }
+
+    @FXML
+    void onWithdrawButtonAction() {
+
+    }
+
+    @FXML
+    void onDepositButtonAction() {
+
+    }
+
+    @FXML
+    void onOpenDepositButtonAction() {
+
+    }
+}
