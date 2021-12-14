9661af6205a9f289f59fa74f2b334db6a17726dc
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-12-14 23:03:14 +0100

    fill account details view with data

--- desktop/src/main/java/xyz/stasiak/cashfx/AccountDetailsController.java
+++ desktop/src/main/java/xyz/stasiak/cashfx/AccountDetailsController.java
@@ -2,8 +2,13 @@ package xyz.stasiak.cashfx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import xyz.stasiak.cashfx.account.AccountApplicationService;
+import xyz.stasiak.cashfx.context.ApplicationContext;
 
 public class AccountDetailsController {
+
+    private final ApplicationState applicationState;
+    private final AccountApplicationService accountApplicationService;
 
     @FXML
     private Label nameLabel;
@@ -14,9 +19,19 @@ public class AccountDetailsController {
     @FXML
     private Label typeLabel;
 
+    public AccountDetailsController() {
+        applicationState = ApplicationContext.CONTEXT.getBean(ApplicationState.class);
+        accountApplicationService = ApplicationContext.CONTEXT.getBean(AccountApplicationService.class);
+    }
+
     @FXML
     void initialize() {
-
+        var accountId = applicationState.getAccountId();
+        var account = accountApplicationService.readAccount(accountId);
+        nameLabel.setText(account.name());
+        moneyLabel.setText(String.format("%d", account.money()));
+        chargeLabel.setText(String.format("%d", account.charge()));
+        typeLabel.setText(account.type().name());
     }
 
     @FXML
